4f4a9bd227161b980337bb0c5d442e5a916f74d8
PSolntsev <PSolntsev@synergy.ru>
2026-04-09 17:20:47 +0300

    test

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
 $(function () {
-  $('[data-target]').on('click', function (e) {
+  $("[data-target]").on("click", function (e) {
     var obj = $(this),
-      target = $('[data-key="' + obj.attr('data-target') + '"]'),
-      headerHeight = $('.head').outerHeight() + 20,
+      target = $('[data-key="' + obj.attr("data-target") + '"]'),
+      headerHeight = $(".head").outerHeight() + 20,
       topValue;
     if (!target.length) {
       topValue = 0;
@@ -12,8 +12,10 @@ $(function () {
       topValue = target.offset().top - headerHeight;
     }
     topValue = Math.floor(topValue);
-    $('html, body').stop().animate({
-      scrollTop: topValue + 'px'
-    });
+    $("html, body")
+      .stop()
+      .animate({
+        scrollTop: topValue + "px",
+      });
   });
 });
